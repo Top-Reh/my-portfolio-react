94126b7a3f0e2066e881e4970b0a1bd6be328ba7
Top <topreh86@gmail.com>
2024-09-04 21:57:28 +0630

    Update pinterestContainer.js

--- src/pinterest/pinterestContainer.js
+++ src/pinterest/pinterestContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route,Link,Routes } from 'react-router-dom';
 import Home from './home';
 import Create from './create';
@@ -6,11 +6,15 @@ import Menu from './menu';
 import './pinterest.css'
 
 const PinterestContainer = () => {
+  const [menuData,setMenuData] = useState();
+  const [firstHome,setFirstHome] = useState('');
+
   return (
     <div className='pinterestbody'>
-        <Menu/>
+        <Menu setMenuData={setMenuData} setFirstHome={setFirstHome}/>
+        {firstHome==='Create' ? <h1>click the home button</h1> : <Home/>}
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/' element={<Home menuData={menuData}/>}/>
         <Route exact path='/Create' element={<Create/>}/>
       </Routes>
     </div>
