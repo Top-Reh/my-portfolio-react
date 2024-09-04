2e60a7ec0579db9b28a2f5208c8731491bb7243b
Top <topreh86@gmail.com>
2024-09-04 21:56:30 +0630

    Update menu.js

--- src/pinterest/menu.js
+++ src/pinterest/menu.js
@@ -1,7 +1,10 @@
 import { useScroll } from 'framer-motion';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route,Link,Routes } from 'react-router-dom';
 import styled from 'styled-components';
+import pinterestData from './pinterestData';
+import Home from './home';
+import Create from './create';
 
 const Btnhm = styled.button`
  background-color: ${({clicked}) => (clicked ==='Home' ? 'black' : 'white')};
@@ -13,9 +16,12 @@ const Btncr = styled.button`
  color: ${({clicked}) => (clicked === 'Create' ? 'white' : 'black')};
 `;
 
-const Menu = () => {
-  const [clicked,setClicked] = useState('Home')
-
+const Menu = ({setMenuData,setFirstHome}) => {
+  const [clicked,setClicked] = useState('Home');
+  const [userName,setUsername] = useState(pinterestData);
+  const [searchresult,setSearchResult] = useState();
+  const [searching,setSearching] = useState('');
+ 
   function btnClickedhome() {
     /*if (!isClickedhm) {
       setIsClickedhm(!isClickedhm);
@@ -23,6 +29,7 @@ const Menu = () => {
       setClicked('Home');
     }*/
     setClicked('Home');
+    setFirstHome('Create');
   }
 
   function btnClickedcreat() {
@@ -31,16 +38,23 @@ const Menu = () => {
       setIsClickedhm(!isClickedhm);
       setClicked('Create')
     }*/
-    setClicked('Create')
+    setClicked('Create');
+    setFirstHome('Create');
   }
+
+  useEffect(()=> {
+    const filtering = userName.filter(name => name.username.toLocaleLowerCase().includes(searching));
+    setSearchResult(filtering);
+    setMenuData(searchresult);
+  },[searching,userName]);
 
   return (
     <div >
       <div className='menu'>
         <i className="bi bi-pinterest pincls"></i>
-        <Link to='/' style={{textDecoration: 'none',border: 'none',margin:'0',fontSize:'16px'}}><Btnhm className='homebtn' onClick={btnClickedhome} clicked={clicked}>Home</Btnhm></Link>
+        <Link to='/' style={{textDecoration: 'none',border: 'none',margin:'0',fontSize:'16px'}} searchresult={searchresult}><Btnhm className='homebtn' onClick={btnClickedhome} clicked={clicked}>Home</Btnhm></Link>
         <Link to='/Create' style={{textDecoration: 'none',border: 'none',margin:'0',fontSize:'16px'}}><Btncr className='createbtn' onClick={btnClickedcreat} clicked={clicked}>Create</Btncr></Link>
-        <input placeholder=' Search'></input>
+        <input placeholder=' Search' value={searching} onChange={(e) => setSearching(e.target.value)}></input>
         <i className="bi bi-bell-fill"></i>
         <i className="bi bi-chat-dots-fill"></i>
         <div className='profileT'>T</div>
