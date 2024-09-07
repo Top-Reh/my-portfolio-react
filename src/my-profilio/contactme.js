b2550a3dc080b942a8e2bab4a023802155a5cb6e
Top <topreh86@gmail.com>
2024-09-07 21:31:31 +0630

    Update contactme.js

--- src/my-profilio/contactme.js
+++ src/my-profilio/contactme.js
@@ -3,27 +3,62 @@ import emailjs from '@emailjs/browser';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-const Contactme = () => {
+const Contact = styled.div `
+    background-color: ${({darkmode}) => (darkmode ? 'rgb(0, 37, 61)' : 'rgb(106, 195, 255)')};
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+const SendInput = styled.input`
+    cursor: pointer;
+    z-index: 400;
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+    border: 2px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Mailform = styled.form`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+    border: 1px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Input= styled.input`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+    border: 1px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Textarea = styled.textarea`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+    border: 1px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Label = styled.label`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Pfp = styled.p`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+    border: 1px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+  const Location = styled.div`
+    color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+  `;
+
+const Contactme = ({darkmode}) => {
+
   const [sent,setsent] = useState(false);
 
   const form = useRef();
 
-  const SendInput = styled.input`
-    cursor: pointer;
-    z-index: 400;
-    border: 2px solid ${(props) => (props.sent ? 'green' : 'blue')};
-  `;
-
   const Sentback = styled.div`
-    width: ${(sent) ? '60%' : '0'};
-    border-radius: 5px;
-    background-color:rgb(23, 88, 152);
-    height:45px;
-    transition: all 2s ease-in-out;
-    position:absolute;
-    bottom:24px;
-    z-index: 4;
-  `
+  width: ${(sent) ? '60%' : '0'};
+  border-radius: 5px;
+  background-color:rgb(23, 88, 152);
+  height:45px;
+  transition: all 2s ease-in-out;
+  position:absolute;
+  bottom:24px;
+  z-index: 4;
+`;
 
   const sendEmail = (e) => {
     setsent(pre => !pre);
@@ -52,121 +87,28 @@ const Contactme = () => {
     setsent(pre => !pre)
   }
   return (
-    <div className='contactme'>
+    <Contact className='contactme' darkmode={darkmode}>
       <div className='locationAndEmail'>
-        <div className='location'>
+        <Location className='location' darkmode={darkmode}>
             <h2>Location</h2>
-            <p>Myanmar , Shan State , Naung Shwe</p>
-        </div>
+            <Pfp darkmode={darkmode}>Myanmar , Shan State , Naung Shwe</Pfp>
+        </Location>
         
-        <form ref={form} onSubmit={sendEmail} className='email'>
-          <label>Your name</label>
-          <input type="text" name="user_name" />
-          <label>Your email</label>
-          <input type="email" name="user_email" />
-          <label>Your message</label>
-          <textarea name="message" />
-          <SendInput type="submit" value={sent ? 'Sent!' : 'Send'} sent={sent}/>
-          <Sentback 
-            animate={{
-              width: sent ? '60%' : '0',
-            }}
-            transition={{
-              type: 'spring',
-              
-            }}
-            sent={sent}
-          />
-        </form>
+        <Mailform ref={form} onSubmit={sendEmail} className='email' darkmode={darkmode}>
+          <Label darkmode={darkmode}>Your name</Label>
+          <Input type="text" name="user_name" darkmode={darkmode}/>
+          <Label darkmode={darkmode}>Your email</Label>
+          <Input type="email" name="user_email" darkmode={darkmode}/>
+          <Label darkmode={darkmode}>Your message</Label>
+          <Textarea name="message" darkmode={darkmode}/>
+          <SendInput type="submit" value={sent ? 'Sent!' : 'Send'} sent={sent} darkmode={darkmode} className='sendbtn'/>
+        </Mailform>
       </div>
       <div className='photo'>
         <img src='https://i.pinimg.com/564x/b3/dd/76/b3dd7688f23e6ba5bd4b78d066f3422f.jpg'></img>
       </div>
-    </div>
+    </Contact>
   )
 }
 
 export default Contactme
-
-/*<div className='email'>
-          <h5>Email</h5>
-          <input type='email' placeholder='Your email to contact'></input>
-          <h5>Phone number</h5>
-          <input placeholder='Your phone number to contact'></input>
-          <h5>Message</h5>
-          <textarea placeholder='some message to give me'></textarea>
-        </div> */
-
-/*
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
-export const ContactUs = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
-
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
-};
-*/
-
-/* import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
-export const ContactUs = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
-
-  return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
-};*/
