a464fa39c56663bfd047a93b82868d553ac821a8
Top <topreh86@gmail.com>
2024-09-07 21:31:06 +0630

    Update profilio.js

--- src/my-profilio/profilio.js
+++ src/my-profilio/profilio.js
@@ -1,30 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dice from '../dice.js';
 
-const Profilio = () => {
+const Did = styled.div `
+background-color: ${({darkmode}) => (darkmode ? 'rgb(0, 37, 61)' : 'rgb(106, 195, 255)')};
+color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+`;
+
+const Hoverbtn = styled.button`
+background-color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+color: ${({darkmode}) => (darkmode ? 'white' : 'white')};
+`;
+
+const Diddiv = styled.div`
+color: ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+border: 2px solid ${({darkmode}) => (darkmode ? 'blue' : 'black')};
+`;
+
+const Profilio = ({darkmode}) => {
+
   return (
-    <div className='did'>
+    <Did className='did' darkmode={darkmode}>
       <h1>What I did</h1>
       <div className='didBlogs'>
-        <div className='mydids'>
-        <Link to='/dice'>Dice</Link>
-        </div>
-        <div className='mydids'>
-        <Link to='/mainBlog'>MainBlog</Link>
-        </div>
-        <div className='mydids'>
-        <Link to='/profiliofast'>Profiliofast</Link>
-        </div>
-        <div className='mydids'>
-        <Link to='/container'>Container</Link>
-        </div>
-        <div className='mydids'>
-          <Link to='/PinterestContainer'>PinterestContainer</Link>
-        </div>
+        <Diddiv className='mydids numbers' darkmode={darkmode}>
+        <Link to='/dice' className='didlinks'>Dice</Link>
+        </Diddiv>
+        <Diddiv className='mydids zeagwat' darkmode={darkmode}>
+        <Link to='/mainBlog' className='didlinks'>Zeagwat</Link>
+        </Diddiv>
+        <Diddiv className='mydids profilefast' darkmode={darkmode}>
+        <Link to='/profiliofast' className='didlinks'>Profiliofast</Link>
+        </Diddiv>
+        <Diddiv className='mydids pinterest' darkmode={darkmode}>
+          <Link to='/PinterestContainer' className='didlinks'>PinterestContainer</Link>
+        </Diddiv>
       </div>
-      <button>View more</button>
-    </div>
+    </Did>
   )
 }
 
